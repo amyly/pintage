84819bb8533c416f381a4bcb37763e52ba548930
amyly <lyamys@gmail.com>
2014-01-12 18:05:31 -0500

    Add Pinboard token field Edit User page, allow users to edit their account without providing a password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :bookmarks
+
+  # def get_pinboard_token(user)
+  #   uri = URI.parse('https://api.pinboard.in/v1/user/api_token/')
+  #   req = Net::HTTP::Get.new uri
+  #   res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request req}
+  #   if res = Net::HTTPUnauthorized
+  #     "boo"
+  #   else
+  #     pinboard_token = Nokogiri::Slop(res.body)
+  #     user.pinboard_token = pinboard_token.result.content
+  #   end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Pintage::Application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  devise_for :users, :controllers => { :registrations => "registrations" }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

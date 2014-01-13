59b941bf0ec9fce4fe3c7c52f26b6dd120a4dd38
amyly <lyamys@gmail.com>
2014-01-12 23:04:57 -0500

    Fix Devise require current password override

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,13 @@ class ApplicationController < ActionController::Base
     before_filter :configure_permitted_parameters, if: :devise_controller?
 
 protected
+
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end
     devise_parameter_sanitizer.for(:account_update) do |u|
-        u.permit(:email, :password, :pinboard_token)
+      u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
     end
   end
 

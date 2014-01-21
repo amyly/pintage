dc94713a13548130507f1ac70630a26fe21526b1
amyly <lyamys@gmail.com>
2014-01-20 21:51:06 -0500

    Add Bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @bookmarks = User.find(current_user.id).bookmarks.where(sent_back: true).order(sent_back_date: :desc)
+    if current_user
+      @bookmarks = User.find(current_user.id).bookmarks.where(sent_back: true).order(sent_back_date: :desc)
+    end
   end
 end
